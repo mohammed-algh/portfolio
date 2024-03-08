cb10644d4ea01e5b36b3f38ad6a32f15f8dcd0ff
Mohammed Alghamdi <mohammedhalgh@hotmail.com>
2024-03-08 14:09:50 +0300

    Refactor URL imports in core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from .views import PortfolioView, ParkingView
+from . import views
 
 urlpatterns = [
-    path('', PortfolioView.as_view(), name='index'),
-    path('parking/', ParkingView.as_view(), name='parking'),
+    path('', views.PortfolioView.as_view(), name='portfolio'),
 ]
