1babcd04bd6ed3478817142e1509795e5209ad01
Mohammed Alghamdi <mohammedhalgh@hotmail.com>
2024-03-08 14:09:59 +0300

    Refactor views.py file

--- core/views.py
+++ core/views.py
@@ -1,14 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
 
-class ParkingView(View):
-    
-    def get(self, request):
-        return render(request, 'parking.html')
-
 class PortfolioView(View):
 
     def get(self, request):
-        return HttpResponse("Hello, world. You're at the portfolio index.")
+        return render(request, 'portfolio.html')
